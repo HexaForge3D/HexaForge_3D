fcfd83f594ef0574d7614da12da0bf2aa51ea5bf
함동현 <haamdonghyun0918@gamil.com>
2026-07-06 15:53:54 +0900

    feat: PlayerController로 마우스 우클릭시 클릭한 곳으로 이동

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Move Setting")]
+    [SerializeField] private float _moveSpeed = 5f; // 이동속도
+    [SerializeField] private float _rotationSpeed = 8f; // 회전속도
+
+    private Vector3 _targetPosition;
+    private bool _isMoving = false;
+
+    private void Start()
+    {
+        _targetPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            SetTargetPosition();
+        }
+
+        if (_isMoving)
+        {
+            MovePlayer();
+        }
+    }
+
+    private void SetTargetPosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            _targetPosition = hit.point;
+
+            _targetPosition.y = transform.position.y;
+
+            _isMoving = true;
+        }
+    }
+
+    private void MovePlayer()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _targetPosition, _moveSpeed * Time.deltaTime);
+        Vector3 direction = (_targetPosition - transform.position).normalized;
+        
+        if (direction != Vector3.zero)
+        {
+            Quaternion lookRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, _rotationSpeed * Time.deltaTime);
+        }
+
+        if (Vector3.Distance(transform.position, _targetPosition) <= 0.05f)
+        {
+            _isMoving = false;
+        }
+    }
+}
