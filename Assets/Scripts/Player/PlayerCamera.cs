d0026b20e0be3839664096bf582207f7a2c16734
나영준 <blueair0379@gmail.com>
2026-07-06 16:40:27 +0900

    feat: [스크립트] 플레이어추적 카메라추가

---
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+public class PlayerCamera : MonoBehaviour
+{
+    public GameObject Target; // 카메라가 따라갈 대상
+
+    public float offsetX = 0f; // 카메라의 X축 오프셋
+    public float offsetY = 10f; // 카메라의 Y축 오프셋
+    public float offsetZ = -10f; // 카메라의 Z축 오프셋
+
+    public float CameraSpeed = 5f; // 카메라 이동 속도
+
+    Vector3 TargetPos;
+
+    private void FixedUpdate()
+    {
+        TargetPos = new Vector3(Target.transform.position.x + offsetX, Target.transform.position.y + offsetY, Target.transform.position.z + offsetZ);
+        transform.position = Vector3.Lerp(transform.position, TargetPos, CameraSpeed * Time.deltaTime * CameraSpeed);
+    }
+}
