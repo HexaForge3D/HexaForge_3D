e44617a632471b6973d6b7b1b6898b6804e83544
권성혁 <gkrrpdl@gmail.com>
2026-07-06 17:43:09 +0900

    [feat] 포탑, 테스트용 플레이어컨트롤러 코드 생성

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    [SerializeField] private string _nextMapName;
+    [SerializeField] private Transform _spawnPoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (MapManager.Instance != null)
+            {
+                MapManager.Instance.ChangeMap(_nextMapName, _spawnPoint.position);
+            }
+            else
+            {
+                Debug.LogError("MapManager.Instance가 null입니다. 씬에 MapManager가 배치되어 있는지 확인하세요.");
+            }
+        }
+    }
+}
