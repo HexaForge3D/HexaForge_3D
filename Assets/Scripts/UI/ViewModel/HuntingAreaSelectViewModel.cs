a3a101edf51a83167179849b6614fd0adf8d5821
이강호 <tiger04176672@gmail.com>
2026-07-07 17:36:05 +0900

    [feat] 사냥터 UI 임시 데이터로 구현

---
+++ Assets/Scripts/UI/ViewModel/HuntingAreaSelectViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class HuntingAreaSelectViewModel
+{
+    private List<HuntingAreaData> _areaList;
+    private HuntingAreaData _selectedArea;
+
+    public Action<HuntingAreaData> OnAreaSelected;
+
+    public Action<HuntingAreaData> OnTeleportRequested;
+
+    public List<HuntingAreaData> GetAreaList()
+    {
+        if (_areaList == null)
+        {
+            _areaList = CreateDummyData();
+        }
+
+        return _areaList;
+    }
+
+    public void SelectArea(HuntingAreaData data)
+    {
+        _selectedArea = data;
+        OnAreaSelected?.Invoke(data);
+    }
+
+    public void RequestTeleport()
+    {
+        if (_selectedArea == null)
+        {
+            return;
+        }
+
+        OnTeleportRequested?.Invoke(_selectedArea);
+    }
+
+    private List<HuntingAreaData> CreateDummyData()
+    {
+        return new List<HuntingAreaData>
+        {
+            new HuntingAreaData("area_a", "사냥터 A", "사냥터A 설명......", ""),
+            new HuntingAreaData("area_b", "사냥터 B", "사냥터B 설명......", ""),
+            new HuntingAreaData("area_c", "사냥터 C", "사냥터C 설명......", ""),
+            new HuntingAreaData("area_d", "사냥터 D", "사냥터D 설명......", ""),
+            new HuntingAreaData("area_e", "사냥터 E", "사냥터E 설명......", ""),
+        };
+    }
+}
