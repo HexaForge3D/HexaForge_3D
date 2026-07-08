6f374461983cd0fa4a05443d3f66c3d9e0abd5ab
김환 <psc00523@gmail.com>
2026-07-08 15:48:18 +0900

    feat: 몬스터 순찰기능 추가
    - 지정범위내에서 랜덤지점 생성 후 자유롭게 순찰

---
+++ Assets/Scripts/RandomPatrolAction.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Unity.Behavior;
+using UnityEngine;
+using UnityEngine.AI;
+using Action = Unity.Behavior.Action;
+using Unity.Properties;
+
+[Serializable, GeneratePropertyBag]
+[NodeDescription(name: "RandomPatrolAction", story: "[Center]를 중심으로 [Radius] 반경 내의 랜덤 위치를 찾아 [ResultPosition]에 저장합니다", category: "Action", id: "random_patrol")]
+public partial class RandomPatrolAction : Action
+{
+    [SerializeReference] public BlackboardVariable<GameObject> Agent;
+    [SerializeReference] public BlackboardVariable<float> Radius;
+    [SerializeReference] public BlackboardVariable<Vector3> Center;
+    [SerializeReference] public BlackboardVariable<Vector3> ResultPosition;
+
+    protected override Status OnStart()
+    {
+        if (Agent.Value == null)
+        {
+            return Status.Failure;
+        }
+        if (Center.Value == Vector3.zero)
+        {
+            Center.Value = Agent.Value.transform.position;
+        }
+
+        Vector3 randomDirection = UnityEngine.Random.insideUnitSphere * Radius.Value;
+        randomDirection += Center.Value;
+
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(randomDirection, out hit, Radius.Value, NavMesh.AllAreas))
+        {
+            ResultPosition.Value = hit.position;
+            return Status.Success;
+        }
+
+        ResultPosition.Value = Center.Value;
+        return Status.Success;
+    }
+}
+
