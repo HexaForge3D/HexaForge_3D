eee51cf878239e57b78d21d9eb31196257380c1e
함동현 <haamdonghyun0918@gamil.com>
2026-07-11 12:27:18 +0900

    feat: 플레이어 카메라에 반투명 기능 추가

--- Assets/Scripts/Player/PlayerCamera.cs
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -1,10 +1,16 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
+
 public class PlayerCamera : MonoBehaviour
 {
-    public GameObject Target; // 카메라가 따라갈 대상
+    [Header("Camera Setting")]
+    [SerializeField] private Transform Target;
+    [SerializeField] private LayerMask _obstacleMask;
+   
     private PlayerController _playerController;
-
     private Vector3 _offset;
+    private List<Renderer> _currentHitRenderers = new List<Renderer>();
+
     private void Start()
     {
         if (Target != null)
@@ -20,5 +26,51 @@ public class PlayerCamera : MonoBehaviour
         Vector3 targetPos = Target.transform.position + _offset;
         float playerSpeed = _playerController.MoveSpeed;
         transform.position = Vector3.Lerp(transform.position, targetPos, playerSpeed * Time.deltaTime);
+
+        CheckOcclusion();
+    }
+
+    private void CheckOcclusion()
+    {
+        Vector3 direction = Target.transform.position - transform.position;
+        float distance = direction.magnitude;
+        Vector3 rayOrigin = transform.position;
+        Vector3 rayDirection = direction.normalized;
+
+        Debug.DrawRay(rayOrigin, rayDirection * distance, Color.yellow, 0.1f);
+
+        List<Renderer> currentHitRenderList = new List<Renderer>();
+
+        foreach (RaycastHit hit in Physics.RaycastAll(rayOrigin, rayDirection, distance, _obstacleMask))
+        {
+            Renderer targetRenderer = hit.collider.GetComponent<Renderer>();
+            if (targetRenderer != null)
+            {
+                currentHitRenderList.Add(targetRenderer);
+
+                if (!_currentHitRenderers.Contains(targetRenderer))
+                {
+                    ChangedHidingObject(targetRenderer, true);
+                }
+            }
+        }
+
+        foreach (Renderer renderer in _currentHitRenderers)
+        {
+            if (currentHitRenderList.Contains(renderer) == false)
+            {
+                ChangedHidingObject(renderer, false);
+            }
+        }
+        _currentHitRenderers = currentHitRenderList;
+    }
+
+    private void ChangedHidingObject(Renderer renderer, bool isTransparent)
+    {
+        if (renderer == null) return;
+
+        Color targetColor = renderer.material.color;
+        targetColor.a = isTransparent ? 0.2f : 1.0f;
+        renderer.material.color = targetColor;
     }
 }
