66bd4e223e5c3e424b0fefa3de41c7e510b495a4
이강호 <tiger04176672@gmail.com>
2026-07-14 14:17:14 +0900

    [refactor] 기능 이전으로 인한 인게임UI 메서드 삭제

--- Assets/Scripts/UI/View/InGameView.cs
+++ Assets/Scripts/UI/View/InGameView.cs
@@ -3,29 +3,11 @@ using UnityEngine.UI;
 
 public class InGameView : BaseUI
 {
-    [SerializeField] private Button Button_BackToCharacterSelect;
-    [SerializeField] private Button Button_HuntingAreaSelect;
 
     private InGameViewModel _viewModel;
 
     public void BindViewModel(InGameViewModel viewModel)
     {
         _viewModel = viewModel;
-
-        Button_BackToCharacterSelect.onClick.RemoveAllListeners();
-        Button_BackToCharacterSelect.onClick.AddListener(OnClickBackToCharacterSelect);
-
-        Button_HuntingAreaSelect.onClick.RemoveAllListeners();
-        Button_HuntingAreaSelect.onClick.AddListener(OnClickHuntingAreaSelect);
-    }
-
-    private void OnClickBackToCharacterSelect()
-    {
-        _viewModel?.RequestBackToCharacterSelect();
-    }
-
-    private void OnClickHuntingAreaSelect()
-    {
-        _viewModel?.RequestHuntingAreaSelect();
     }
 }
--- Assets/Scripts/UI/ViewModel/InGameViewModel.cs
+++ Assets/Scripts/UI/ViewModel/InGameViewModel.cs
@@ -1,18 +1,6 @@
 ﻿using System;
-using UnityEngine;
 
 public class InGameViewModel
 {
-    public Action OnBackToCharacterSelectRequested;
-    public Action OnHuntingAreaSelectRequested;
 
-    public void RequestBackToCharacterSelect()
-    {
-        OnBackToCharacterSelectRequested?.Invoke();
-    }
-
-    public void RequestHuntingAreaSelect()
-    {
-        OnHuntingAreaSelectRequested?.Invoke();
-    }
 }
