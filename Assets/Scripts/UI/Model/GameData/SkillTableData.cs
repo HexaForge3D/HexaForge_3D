3bf0807b9fe57426dc8b2d42658fbc592dc829de
이강호 <tiger04176672@gmail.com>
2026-07-14 15:03:52 +0900

    [feat] 스킬 데이터 템플릿 작성

---
+++ Assets/Scripts/UI/Model/GameData/SkillTableData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+[Serializable]
+public class SkillTableData : GameDataBase
+{
+    public string Name;
+    public string Key;
+    public int RequiredLevel;
+    public string IconAddress;
+    public float CoolDown;
+    public int Damage;
+    public int ManaCost;
+}
---
+++ Assets/Scripts/UI/Model/SkillData.cs
@@ -0,0 +1,19 @@
+﻿public class SkillData
+{
+    public string Id {  get; private set; }
+    public string Name { get; private set; }
+    public string Key { get; private set; }
+    public int RequiredLevel { get; private set; }
+    public string IconAddress {  get; private set; }
+    public float CoolDown { get; private set; }
+
+    public SkillData(string id, string name, string key, int requiredLevel, string iconAddress, float coolDown)
+    {
+        Id = id;
+        Name = name;
+        Key = key;
+        RequiredLevel = requiredLevel;
+        IconAddress = iconAddress;
+        CoolDown = coolDown;
+    }
+}
