e05db27a218e4fb68789b82e1b0786171f4071c5
이강호 <tiger04176672@gmail.com>
2026-07-15 13:18:10 +0900

    [feat] 임시 아이템 데이터를 받아올 레포지토리 작성

---
+++ Assets/Scripts/UI/Repository/ItemRepository.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemRepository
+{
+    public List<ItemData> GetAllItems()
+    {
+        Dictionary<string, ItemTableData> table = GameDataManager.Instance.GetAllData<ItemTableData>();
+        List<ItemData> result = new List<ItemData>();
+
+        if (table == null) return result;
+
+        foreach (ItemTableData tableData in table.Values)
+        {
+            result.Add(ConvertToItemData(tableData));
+        }
+
+        return result;
+    }
+
+    public ItemData GetItem(string id)
+    {
+        ItemTableData tableData = GameDataManager.Instance.GetData<ItemTableData>(id);
+        return tableData == null ? null : ConvertToItemData(tableData);
+    }
+
+    private ItemData ConvertToItemData(ItemTableData tableData)
+    {
+        return new ItemData(
+            tableData.ID,
+            tableData.Name,
+            tableData.Description,
+            tableData.IconAddress,
+            tableData.MaxStack,
+            tableData.UsageType
+            );
+    }
+}
