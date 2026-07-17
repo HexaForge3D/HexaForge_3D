999094f58c9222b3765e74813e46c3ce6fc968a4
이주헌 <Volume6214@gmail.com>
2026-07-17 13:58:27 +0900

    feat: NPC 스크립트 추가

---
+++ Assets/Scripts/NPC/NPC.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using UnityEngine;
+
+public enum NPCId : byte
+{
+    None,
+    Store,
+    MainQuest,
+    Smithy
+}
+
+public class NPC : MonoBehaviour
+{
+    private bool _isPlayerInCollider;
+
+    public static event Action<NPC> OnNPCInteracted;
+
+    [SerializeField] private NPCId _npcId;
+
+    public NPCId NPCId
+    {
+        get
+        {
+            return _npcId;
+        }
+    }
+
+    private void OnEnable()
+    {
+        PlayerInputSystem.OnInteract += HandleInteraction;
+    }
+
+    private void OnDisable()
+    {
+        PlayerInputSystem.OnInteract -= HandleInteraction;
+    }
+
+    public void RequestInteraction()
+    {
+        OnNPCInteracted?.Invoke(this);
+    }
+
+    //public void Interact()
+    //{
+    //    UIManager.Instance.ShowNPCUI(this.NPCId);
+    //}
+
+    private void HandleInteraction()
+    {
+        if (_isPlayerInCollider == false)
+        {
+            return;
+        }
+
+        RequestInteraction();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _isPlayerInCollider = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _isPlayerInCollider = false;
+        }
+    }
+}
