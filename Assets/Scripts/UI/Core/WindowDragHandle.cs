9867353a018dd58db057aa75eb163533b57ebc27
이강호 <tiger04176672@gmail.com>
2026-07-18 21:16:41 +0900

    [feat] 창 드래그 핸들러 추가

---
+++ Assets/Scripts/UI/Core/WindowDragHandle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class WindowDragHandle : MonoBehaviour, IDragHandler
+{
+    [SerializeField] private RectTransform TargetRectTransform;
+
+    private void Awake()
+    {
+        if (TargetRectTransform == null)
+        {
+            TargetRectTransform = transform.parent as RectTransform;
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        TargetRectTransform.anchoredPosition += eventData.delta;
+    }
+}
