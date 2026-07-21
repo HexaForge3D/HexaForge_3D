ea94dd0f63f7fe2a8130e9cbdcc04d641c571609
나영준 <blueair0379@gmail.com>
2026-07-21 14:22:25 +0900

    feat: [사운드] 사운드 매니저 메서드추가,사운드 옵션 슬라이더코드 추가,오디오믹서 추가

--- Assets/Scripts/UI/View/GameMenuView.cs
+++ Assets/Scripts/UI/View/GameMenuView.cs
@@ -6,6 +6,11 @@ public class GameMenuView : BaseOverLayUI
     [SerializeField] private Button Button_CharacterSelect;
     [SerializeField] private Button Button_QuitGame;
 
+    [Header("Volume Sliders")]
+    [SerializeField] private Slider Slider_BGM;
+    [SerializeField] private Slider Slider_SFX;
+    [SerializeField] private Slider Slider_UI;
+
     private GameMenuViewModel _viewModel;
 
     public void BindViewModel(GameMenuViewModel viewModel)
@@ -17,6 +22,23 @@ public class GameMenuView : BaseOverLayUI
 
         Button_QuitGame.onClick.RemoveAllListeners();
         Button_QuitGame.onClick.AddListener(OnClickQuitGame);
+
+
+        if (Slider_BGM != null)
+        {
+            Slider_BGM.onValueChanged.RemoveAllListeners();
+            Slider_BGM.onValueChanged.AddListener(OnChangeBGM);
+        }
+        if (Slider_SFX != null)
+        {
+            Slider_SFX.onValueChanged.RemoveAllListeners();
+            Slider_SFX.onValueChanged.AddListener(OnChangeSFX);
+        }
+        if (Slider_UI != null)
+        {
+            Slider_UI.onValueChanged.RemoveAllListeners();
+            Slider_UI.onValueChanged.AddListener(OnChangeUI);
+        }
     }
 
     private void OnClickCharacterSelect()
@@ -27,5 +49,20 @@ public class GameMenuView : BaseOverLayUI
     private void OnClickQuitGame()
     {
         _viewModel?.RequestQuitGame();
+    }
+
+    private void OnChangeBGM(float value)
+    {
+        _viewModel?.ChangeBGMVolume(value);
+    }
+
+    private void OnChangeSFX(float value)
+    {
+        _viewModel?.ChangeSFXVolume(value);
+    }
+
+    private void OnChangeUI(float value)
+    {
+        _viewModel?.ChangeUIVolume(value);
     }
 }
