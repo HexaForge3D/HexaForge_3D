e4f153747022a17d0eb8d4967957767a5a1a5ae2
권성혁 <gkrrpdl@gmail.com>
2026-07-22 18:31:11 +0900

    [feat] 마을 프리팹에 표지판 추가, 표지판 스크립트 추가

---
+++ Assets/Scripts/Map/Sign.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+public class Sign : MonoBehaviour
+{
+    private string _mapPrefabPath = string.Empty;
+    private bool _playerInRange = false;
+
+    public static Action OnShowMapRequest;
+
+    private void OnEnable()
+    {
+        _mapPrefabPath = "";
+        PlayerInputSystem.OnInteract += HandleInteraction;
+    }
+
+    private void OnDisable()
+    {
+        PlayerInputSystem.OnInteract -= HandleInteraction;
+    }
+
+    private void HandleInteraction()
+    {
+        if (_playerInRange)
+        {
+            OnShowMapRequest?.Invoke();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _playerInRange = true;
+            Debug.Log("맵을 보려면 상호작용을 누르세요"); // [TODO] 추후 팝업이나 아이콘으로 띄울 예정
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _playerInRange = false;
+        }
+    }
+}
