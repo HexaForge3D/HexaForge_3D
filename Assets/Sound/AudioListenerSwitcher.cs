d893259b617495b3757a51ecb4c65f5d9fd33274
나영준 <blueair0379@gmail.com>
2026-07-22 15:21:24 +0900

    feat: [사운드] 카메라의 Listener가 두개이상 켜지지않도록 스크립트 추가

---
+++ Assets/Sound/AudioListenerSwitcher.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class AudioListenerSwitcher : MonoBehaviour
+{
+    private AudioListener _mainCameraListener;
+
+    private void Start()
+    {
+        GameObject mainCam = GameObject.FindGameObjectWithTag("MainCamera");
+
+        if (mainCam != null)
+        {
+            _mainCameraListener = mainCam.GetComponent<AudioListener>();
+
+            if (_mainCameraListener != null)
+            {
+                _mainCameraListener.enabled = false;
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (_mainCameraListener != null)
+        {
+            _mainCameraListener.enabled = true;
+        }
+    }
+}
