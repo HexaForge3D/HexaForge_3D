d36a35a321564f2cd7c386c414317b709bf40726
나영준 <blueair0379@gmail.com>
2026-07-22 15:21:56 +0900

    feat: [사운드] 맵 전용 사운드 스크립트 추가

---
+++ Assets/Sound/MapBGM.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class MapBGM : MonoBehaviour
+{
+    [Header("Map BGM Settings")]
+    [SerializeField] private AudioClip _mapBGMClip;
+
+    [SerializeField] private string _bgmFileName;
+
+    [Range(0f, 1f)]
+    [SerializeField] private float _bgmVolume = 1f;
+
+    private void Start()
+    {
+        if (SoundManager.Instance == null)
+        {
+            Debug.LogError("SoundManager instance is not found in the scene.");
+            return;
+        }
+        if (_mapBGMClip != null)
+        {
+            SoundManager.Instance.PlayBGM(_mapBGMClip, _bgmVolume);
+        }
+        else if (!string.IsNullOrEmpty(_bgmFileName))
+        {
+            SoundManager.Instance.PlayBGMSound(_bgmFileName);
+        }
+
+    }
+}
