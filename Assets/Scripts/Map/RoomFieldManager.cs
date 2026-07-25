4f1d9b210ac229b697d215032696025930654e74
이주헌 <Volume6214@gmail.com>
2026-07-25 11:30:02 +0900

    refactor: 불필요한 코드 수정

--- Assets/Scripts/Map/RoomFieldManager.cs
+++ Assets/Scripts/Map/RoomFieldManager.cs
@@ -147,26 +147,8 @@ public class RoomFieldManager : BaseDungeonController
         _isCleared = true;
         Debug.Log("룸 필드의 모든 몬스터를 처치했습니다! 필드 클리어!");
 
-        DungeonReward reward = CreateReward();
-
-        OnClearField?.Invoke();
-        InvokeCleared(reward);
-
         ClearDungeon();
     }
-
-    //private void HandleFieldFail()
-    //{
-    //    if (_isCleared || _isFailed)
-    //    {
-    //        return;
-    //    }
-
-    //    _isFailed = true;
-    //    Debug.Log("룸 필드 클리어 실패");
-
-    //    FailDungeon();
-    //}
 
     private void ClearDungeon()
     {
